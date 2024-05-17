78ea6b024f60a8e99ee8a9cbe3ba45750874c6c3
zchisholm <shakwanc21@gmail.com>
2024-05-16 22:19:40 -0400

    Updated before_isntall.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,7 +5,7 @@ cd /app
 
 # install node and npm
 apt-get install curl
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 apt-get install nodejs -y
 apt-get install npm -y
 apt install nginx -y
