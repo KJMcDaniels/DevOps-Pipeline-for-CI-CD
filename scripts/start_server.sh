aa1c7ac2c77e1492d624b00923487cd2321b709a
zchisholm <shakwanc21@gmail.com>
2024-05-16 21:19:22 -0400

    Updated start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # # navigate to backend folder
-cd /opt/codedeploy-agent/backend
+cd /app/backend
 
 systemctl -l enable nginx
 systemctl -l start nginx
-node backend/index.js
+node index.js
