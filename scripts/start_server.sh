86d0178304a4c96405d5d4346a57064842af94aa
zchisholm <shakwanc21@gmail.com>
2024-05-16 21:06:28 -0400

    Updated start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-# # navigate to app folder
-# cd app
+# # navigate to backend folder
+cd /opt/codedeploy-agent/backend
 
-# # initial startup by running react-script "start", name process "marketing"
-# # --watch watches and restarts if files change
-# npm start
-# pm2 start
-# pm2 startup
-# pm2 save
-# pm2 restart all
 systemctl -l enable nginx
 systemctl -l start nginx
 node backend/index.js
