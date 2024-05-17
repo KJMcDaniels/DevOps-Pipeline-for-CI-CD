73c863dd1319008822f5537d48627f03d88c4471
Opeyemi Olaleye <o.immanuel.o@gmail.com>
2024-05-17 00:17:42 -0400

    updated validate_service.sh script

--- pipeline_script/validate_service.sh
+++ pipeline_script/validate_service.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # Validate the service is running
-# You can replace this with your own validation logic
 
 SERVICE_URL="http://localhost"
 
-STATUS_CODE=$(curl --write-out %{http_code} --silent --output /dev/null $SERVICE_URL)
+STATUS_CODE=$(curl --write-out %{http_code} --silent $SERVICE_URL)
 
 if [ "$STATUS_CODE" -ne 200 ]; then
   echo "Validation failed with status code $STATUS_CODE"
